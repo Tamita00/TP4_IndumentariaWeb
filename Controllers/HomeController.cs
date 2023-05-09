9479dff74abae0757b22e198d2c5e4c70d938e6d
TAMARA YAEL DRATLER <47807159@ort>
2023-05-09 11:07:24 -0300

    Subiendo ultima version

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,21 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        ViewBag.EquiposIndumentaria = Equipos.EquiposIndumentaria;
+        return View("Index");
+    }
+    public IActionResult SeleccionarIndumentaria()
+    {
+        ViewBag.listaEquipos = Equipos.listaEquipos;
+        ViewBag.listaRemeras = Equipos.listaRemeras;
+        ViewBag.listaPantalones = Equipos.listaPantalones;
+        ViewBag.listaMedias = Equipos.listaMedias;
+        return View("SeleccionarIndumentaria");
+    }
+
+    public IActionResult GuardarIndumentaria (int Equipo, int Media, int Pantalon, int Remera){
+        Indumentaria nuevoEquipoConjunto = new Indumentaria(Equipos.listaMedias[Media - 1], Equipos.listaPantalones[Pantalon - 1], Equipos.listaRemeras[Remera - 1]);
+        if(Equipos.IngresarIndumentaria(Equipos.listaEquipos[Equipo - 1], nuevoEquipoConjunto) == false) ViewBag.MensajeError = "Hubo un error";
+        return RedirectToAction("Index");
     }
 }
--- Models/Equipos.cs
+++ Models/Equipos.cs
@@ -1,12 +1,16 @@
-public static class Equipo{
+public static class Equipos{
     public static List<string> listaEquipos {get; private set;} = new List<string>(){"River", "Boca", "San Martín de Tucumán", "Nueva Chicago", "Depertivo Riestra", "Guillermo Brown", "Deportivo Maipú", "Villa Dalmine", "Chaco For Ever", "Justo José de Urquiza"};
-    public static List<string> listaMedias {get; private set;}
-    public static List<string> listaPantalones {get; private set;}
-    public static List<string> listaRemeras {get; private set;}
-    public static Dictionary<string, Indumentaria> EquiposIndumentaria {get; private set;}
+    public static List<string> listaMedias {get; private set;} = new List<string>(){"medias1.jpg", "medias2.jpg", "medias3.jpg", "medias4.jpg","medias5.jpg", "medias6.jpg", "medias7.jpg", "medias8.jpg", "medias9.jpg", "medias10.jpg"};
+    public static List<string> listaPantalones {get; private set;} = new List<string>(){"short1.jpg", "short2.jpg", "short3.jpg", "short4.jpg","short5.jpg", "short6.jpg", "short7.jpg", "short8.jpg", "short9.jpg", "short10.jpg"};
+    public static List<string> listaRemeras {get; private set;} = new List<string>(){"remera1.jpg", "remera2.jpg", "remera3.jpg", "remera4.jpg","remera5.jpg", "remera6.jpg", "remera7.jpg", "remera8.jpg", "remera9.jpg", "remera10.jpg"};
+    public static Dictionary<string, Indumentaria> EquiposIndumentaria {get; private set;} = new Dictionary<string, Indumentaria>();
 
-    public static bool IngresarIndumentaria(string EquipoSeleccionado, Indumentaria cargar){
-        if(EquiposIndumentaria.ContainsKey(EquipoSeleccionado)) return true;
+    public static bool IngresarIndumentaria(string EquipoSeleccionado, Indumentaria conjunto){
+        if(!EquiposIndumentaria.ContainsKey(EquipoSeleccionado))
+        {
+            EquiposIndumentaria.Add(EquipoSeleccionado, conjunto);
+            return true;
+        } 
         else return false;
     }
 
